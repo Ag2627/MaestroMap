4372a0b96dbdb9b7722832d2bc045a6ee1499b2e
abhaysachan01 <abhaysachan360@gmail.com>
2026-03-02 18:33:40 +0530

    serverless enhacement

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,14 +1,12 @@
-// config/db.js
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("MongoDB connection error:", error.message);
-    process.exit(1);
-  }
+  if (isConnected) return;
+
+  const db = await mongoose.connect(process.env.MONGO_URI);
+  isConnected = db.connections[0].readyState;
 };
 
 export default connectDB;
--- Server/server.js
+++ Server/server.js
@@ -1,20 +1,14 @@
 import app from './app.js';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-// import cron from "node-cron";
-// import runScraper from "./Scrapper/Scrapper.js"
-
-
-// cron.schedule("0 0 * * *", () => {
-//   console.log("Running daily scraper...");
-//   runScraper();
-// });
-
 dotenv.config();
+import connectDB from './config/db.js';
+
 
 connectDB();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`MaestroMap server running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`MaestroMap server running on port ${PORT}`);
+// });
+// console.log(process.env.MONGO_URI)
+export default app;
